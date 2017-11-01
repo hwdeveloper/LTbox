34e51bb8601c47dfd5d28d5e66fb21e2dddb9210
hw_developer <hw_developer@163.com>
2017-11-01 10:37:34 +0800

    add the file

---
+++ application/views/nav.php
@@ -0,0 +1,32 @@
+<div class="header-bottom">
+<div class="container">
+<div class="h_menu4">
+	<a class="toggleMenu" href="#">Menu</a>
+
+	<ul class="nav">
+		<li ><a href="<?php echo site_url('HomeController')?>"><i> </i>Desktops</a></li>
+		<li class="active"><a href="#" >Laptops & Notebooks</a>
+			<ul class="drop">
+				<li><a href="<?php echo site_url('ProductController')?>">Sony(2)</a></li>
+				<li><a href="<?php echo site_url('ProductController')?>">Android(4)</a></li>
+				<li><a href="<?php echo site_url('ProductController')?>">Apple(7)</a></li>
+				<li><a href="<?php echo site_url('ProductController')?>">Acer(53)</a></li>
+				<li><a href="<?php echo site_url('ProductController')?>">HP(78)</a></li>
+				<li><a href="<?php echo site_url('ProductController')?>">Intel(5)</a></li>
+			</ul>
+		</li>
+		<li><a href="<?php echo site_url('ProductController')?>" >  Tablets</a></li>
+		<li><a href="<?php echo site_url('ProductController')?>" >Components</a></li>
+		<li><a href="<?php echo site_url('ProductController')?>" >Software</a></li>
+		<li><a href="<?php echo site_url('ProductController')?>" >Phones & PDAs </a></li>
+		<li><a href="<?php echo site_url('ProductController')?>" >  Cameras  </a></li>
+		<li><a href="<?php echo site_url('ProductController')?>" >Contact </a></li>
+
+	</ul>
+
+
+	<script type="text/javascript" src="<?php echo base_url()?>js/nav.js"></script>
+</div>
+
+</div>
+</div>
